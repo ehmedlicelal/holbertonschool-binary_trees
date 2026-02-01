549f42351ea12bed8d70ca0d23655ea098390e1a
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-02-01 12:28:57 +0000

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		if (tree->left || tree->right)
 			return (1);
-		return (binary_tree_nodes(tree->left) + binary_tree_nodess(tree->right));
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	}
 	return (0);
 }
