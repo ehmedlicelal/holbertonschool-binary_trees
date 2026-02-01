add09bb73b543258ed97059cdf78ddaa2ab88096
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-02-01 10:35:39 +0000

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,13 +8,7 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
-
 	if (tree)
-	{
-		size = tree->left ? 1 + binary_tree_size(tree->left) : 0;
-		size = tree->right ? 1 + binary_tree_size(tree->right) : 0;
-		return (size);
-	}
+		return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 	return (0);
 }
